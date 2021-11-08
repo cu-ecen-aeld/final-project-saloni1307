6acc634a56921ec18c8dc6eefd85dabc16ea144e
saloni1307 <sash6289@colorado.edu>
2021-11-07 23:21:29 -0700

    update: added mesa and eudev, still getting uninitialized error in camera

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = fa92e58b9adac1a006259174a6a87613151b2b1b
+AESD_ASSIGNMENTS_VERSION = f69d1be78660333133b8dfe831e0fec7cac0a7a5
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -13,13 +13,15 @@ AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/final-project-Mich2899.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
-#define AESD_ASSIGNMENTS_BUILD_CMDS
-#	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
-#endef
+define AESD_ASSIGNMENTS_BUILD_CMDS
+#	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/obd2.py $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/car.py $(TARGET_DIR)/usr/bin/
+
+
 endef
 
 $(eval $(generic-package))
