ca7b08f6e1fae79924ed72cae8a5ef84e93a3122
saloni1307 <sash6289@colorado.edu>
2021-11-21 20:38:20 -0700

    GUI working, face_recog working with GUI

--- base_external/rootfs_overlay/etc/project/face_recog.py
+++ base_external/rootfs_overlay/etc/project/face_recog.py
@@ -47,8 +47,8 @@ face_encodings = []
 face_names = []
 process_this_frame = True
 
-fps = video_capture.get(cv2.CAP_PROP_FPS)
-print("FPS: {0}".format(fps))
+#fps = video_capture.get(cv2.CAP_PROP_FPS)
+#print("FPS: {0}".format(fps))
 #######################################################################################
 ################infinite loop to recognise face in the frame of the camera############
 while True:
@@ -91,36 +91,37 @@ while True:
                        #print("Alert!! Alert!! Driver Drowsiness Detected")
                        #cv2.putText(frame, "DROWSINESS ALERT!", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-            #face_names.append(name)
+            face_names.append(name)
             print(name)   
 
 
     process_this_frame = not process_this_frame
     
         # Display the results
-    #for (top, right, bottom, left), name in zip(face_locations, face_names):
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
             # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-       # top *= 4
-       # right *= 4
-       # bottom *= 4
-       # left *= 4
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
 
             # Draw a box around the face
-       # cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
             # Draw a label with a name below the face
-       # cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-       # font = cv2.FONT_HERSHEY_DUPLEX
-      #  cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
         
 
         # Display the resulting image
-   # cv2.imshow('Video', frame)
+    cv2.imshow('Video', frame)
     
     
         # Hit 'q' on the keyboard to quit!
-   # if cv2.waitKey(1) & 0xFF == ord('q'):
-     # break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
       
-#video_capture.release()
-#cv2.destroyAllWindows()
+video_capture.release()
+cv2.destroyAllWindows()
+
